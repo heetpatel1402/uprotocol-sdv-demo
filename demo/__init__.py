94443d572fec40a47cb0cf6da0f31cbb0b2429cd
heetpatel1402 <78435053+heetpatel1402@users.noreply.github.com>
2025-11-23 13:03:39 -0500

    Update __init__.py

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,1 +1,17 @@
 
+# demo/__init__.py
+"""
+Makes the 'demo' directory a Python package
+and exposes all demo modules for pytest imports.
+"""
+
+from .common import *
+from .config_store import *
+from .alert_service import *
+from .rpc_server import *
+from .rpc_client import *
+from .status_summary import *
+from .pub_telemetry import *
+from .geofence_service import *
+from .driver_behavior import *
+from .history_utils import *
